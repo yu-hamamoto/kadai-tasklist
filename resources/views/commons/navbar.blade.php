c5b21af794831e319f0ec564be3269c1364e7298
yu.hamamoto <yu.hamamoto@outlook.jp>
2020-06-21 08:44:57 +0000

    user registration

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,6 @@
+<ul class="nav navbar-nav navbar-right">
+    {{--ユーザ登録ページへのリンク--}}
+    <li>{!! link_to_route('signup.get','signup',[],['class'=>'nav-link']) !!}</li>
+    {{--ログインページへのリンク--}}
+    <li><a href="#">Login</a></li>
+</ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the tasklist</h1>
+            {{--ユーザ登録ページへのリンク--}}
+            {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-lg btn-primary']) !!} 
+        </div>
+    </div>
+@endsection
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
